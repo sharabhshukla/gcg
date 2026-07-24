a945294b003ecc483a67bdae1dfd552513aa0e12
Erik Mühmer <muehmer@or.rwth-aachen.de>
2026-07-24 20:41:51 +0200

    increase version, update CHANGELOG

--- src/gcg/def.h
+++ src/gcg/def.h
@@ -60,7 +60,7 @@
 #endif
 #endif
 
-#define GCG_VERSION         402 /**< GCG version number (multiplied by 100 to get integer number) */
+#define GCG_VERSION         403 /**< GCG version number (multiplied by 100 to get integer number) */
 #define GCG_SUBVERSION        0 /**< GCG sub version number */
 
 #endif
